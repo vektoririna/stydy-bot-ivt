8bfd5dad7ac45510a54d7a2248de85a36c8af14b
vektoririna <60616025+vektoririna@users.noreply.github.com>
2021-06-03 12:23:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import telebot
+bot = telebot.TeleBot('1685435465:AAHcVtYrPgdi98zJXjMJ0P9VMiV971uYQso')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+
+bot.polling(none_stop=True)
